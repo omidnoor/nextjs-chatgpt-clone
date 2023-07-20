66422d1d45482dd189fa6b2a51ffe61b46bd21f3
Omid Noorshams <o.noorshams@gmail.com>
2023-07-19 18:39:15 -0700

    sendMessage api update

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -1,3 +1,5 @@
+import { OpenAIEdgeStream } from "openai-edge-stream";
+
 export const config = {
   runtime: "edge",
 };
@@ -5,7 +7,28 @@ export const config = {
 export default async function handler(req, res, next) {
   try {
     const { message } = await req.json();
-    console.log(message);
+    const stream = await OpenAIEdgeStream(
+      `https://api.openai.com/v1/chat/completions`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              content: message,
+              role: "user",
+            },
+          ],
+          stream: true,
+          temprature: 0.9,
+        }),
+      }
+    );
+    return new Response(stream);
   } catch (error) {
     console.error("Error: ", error);
     // return res.status(400).json({ error: error.message });

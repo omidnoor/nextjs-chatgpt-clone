1222fa86090a0c7834a9e137ea81074b7ebbc52e
Omid Noorshams <o.noorshams@gmail.com>
2023-07-19 17:10:29 -0700

    setup login / logout

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { isLoading, error, user } = useUser();
+
   return (
     <div>
       <Head>
         <title>Next JS ChatGPT Starter</title>
       </Head>
       <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-      <Link href="/api/auth/login">Login</Link>
+      <div>
+        {!!user && <Link href="/api/auth/logout">Logout</Link>}
+        {!user && <Link href="/api/auth/login">Login</Link>}
+      </div>
     </div>
   );
 }

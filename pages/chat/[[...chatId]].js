c7da0df0d50214bc455321476e0c06d440790004
Omid Noorshams <o.noorshams@gmail.com>
2023-07-19 18:09:31 -0700

    update chatpage

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,7 +1,15 @@
 import ChatSidebar from "components/ChatSidebar";
 import Head from "next/head";
+import { useState } from "react";
 
-export default function Home() {
+export default function ChatPage() {
+  const [messageText, setMessageText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(messageText);
+  };
+
   return (
     <>
       <Head>
@@ -10,8 +18,22 @@ export default function Home() {
       <div className="grid h-screen grid-cols-[260px_1fr]">
         <ChatSidebar />
         <div className="flex flex-col bg-gray-700">
-          <div className="flex flex-1 flex-col">chat window</div>
-          <footer className=" bg-gray-800 p-10">footer</footer>
+          <div className="flex-1 ">chat window</div>
+          <footer className=" bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="flex gap-2">
+                <textarea
+                  value={messageText}
+                  onChange={(e) => setMessageText(e.target.value)}
+                  placeholder="Send a message..."
+                  className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline-emerald-500"
+                />
+                <button type="submit" className="btn">
+                  Send
+                </button>
+              </fieldset>
+            </form>
+          </footer>
         </div>
       </div>
     </>

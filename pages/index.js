d74ee9afa9fb44ed5b5ce6f2e9b3f79ea2399adc
Omid Noorshams <o.noorshams@gmail.com>
2023-07-22 17:23:44 -0700

    update home page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { getSession } from "@auth0/nextjs-auth0";
+import { BsRobot } from "react-icons/bs";
 
 export default function Home() {
   const { isLoading, error, user } = useUser();
@@ -12,18 +13,24 @@ export default function Home() {
         <title>MemoAI - Login or Signup</title>
       </Head>
       <div className="flex min-h-screen w-full items-center justify-center gap-2 bg-gray-800 text-center text-white">
-        <div>
-          {!!user && <Link href="/api/auth/logout">Logout</Link>}
-          {!user && (
-            <>
-              <Link href="/api/auth/login" className="btn">
-                Login
-              </Link>
-              <Link href="/api/auth/signup" className="btn">
-                Signup
-              </Link>
-            </>
-          )}
+        <div className="">
+          <div className="mb-2 flex items-center justify-center">
+            <BsRobot size={70} className="p-1 text-emerald-200" />
+          </div>
+          <h1 className="mb-2 text-4xl font-bold">Welcom to MemoAI</h1>
+          <p className="mt-2 text-lg">Log in with your accoutn to continue</p>
+          <div className="mt-4 flex justify-center gap-3">
+            {!user && (
+              <>
+                <Link href="/api/auth/login" className="btn">
+                  Login
+                </Link>
+                <Link href="/api/auth/signup" className="btn">
+                  Signup
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>

a8c9b5d0ff79e41c306f14b06d3c69170aaf1201
Omid Noorshams <o.noorshams@gmail.com>
2023-07-22 19:02:44 -0700

    validation serverside addMessageToChat

--- pages/api/chat/addMessageToChat.js
+++ pages/api/chat/addMessageToChat.js
@@ -8,10 +8,35 @@ export default async function handler(req, res) {
     const client = await clientPromise;
     const db = client.db("memoai");
     const { chatId, role, content } = req.body;
+    let objectId;
+    try {
+      objectId = new ObjectId(chatId);
+    } catch (error) {
+      res.status(422).json({ message: "Invalid chat id!" });
+    }
+
+    if (
+      !content ||
+      typeof content !== "string" ||
+      (role === "user" && content.length > 200) ||
+      (role === "assistant" && content.length > 100000)
+    ) {
+      res.status(422).json({
+        message: "Content is required and must be less than 200 characters",
+      });
+      return;
+    }
+
+    if (role !== "user" && role !== "assistant") {
+      res
+        .status(422)
+        .json({ message: "Role must be assistant, user or system!" });
+      return;
+    }
 
     const chat = await db.collection("chats").findOneAndUpdate(
       {
-        _id: new ObjectId(chatId),
+        _id: objectId,
         userId: user.sub,
       },
       {
